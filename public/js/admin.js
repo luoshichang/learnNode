cbb5fdc534bf18e27f8f1366e0e90274ecfb8511
luoshichang <1245luo@gmail.com>
2016-07-18 15:44:11 +0800

    利用豆瓣API同步电影数据

--- public/js/admin.js
+++ public/js/admin.js
@@ -14,4 +14,30 @@ $(function(){
             }
         })
     })
+    $('#douban').blur(function(){
+        var douban = $(this)
+        var id = douban.val()
+        if(id){
+            $.ajax(
+            {
+                url:'https://api.douban.com/v2/movie/subject/'+id,
+                cache:true,
+                type:'get',
+                dataType:'jsonp',
+                crossDomain:true,
+                jsonp:'callback',
+                success:function(data){
+                    $('#inputTitle').val(data.title)
+                    $('#inputDoctor').val(data.directors[0].name)
+                    $('#inputCountry').val(data.countries[0])
+                    //$('#inputLanguage').val(data)
+                    $('#inputPoster').val(data.images.large)
+                    $('#inputYear').val(data.year)
+                    $('#inputSummary').val(data.summary)
+
+                }
+            }
+        )}
+
+    })
 })
